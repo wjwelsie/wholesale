42878d8fc1488c6aec0f84c33678e8f06ee4d1ad
wjwelsie <44932782+wjwelsie@users.noreply.github.com>
2022-02-03 20:34:01 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # -cp: Class Path
-
+# 当java以 com.anz.wholesale.WholesaleApplication 为入口文件运行整个程序的时候
+# 所依赖的 dependency class 都在 app/lib/ 路径之下
+# app/lib 是在 Dockerfile 下根据 image 定义好的
 java -cp app:app/lib/* com.anz.wholesale.WholesaleApplication \
 
 # src/main/resources/application.properties 定义变量
