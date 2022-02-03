62d3550042f65a8f87b9f9e799ea32267d5ceaf3
wjwelsie <44932782+wjwelsie@users.noreply.github.com>
2022-02-03 17:55:08 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# -cp: Class Path
+
 java -cp app:app/lib/* com.anz.wholesale.WholesaleApplication \
+
+# src/main/resources/application.properties 定义变量
           -Dspring.datasource.url=${SPRING_DATASOURCE_URL} \
           -Dspring.datasource.username=${SPRING_DATASOURCE_USERNAME} \
           -Dspring.datasource.password=${SPRING_DATASOURCE_PASSWORD}
